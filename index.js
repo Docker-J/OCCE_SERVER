f02acfd2c81c57128a7288b1940de77ae4ae7984
Junesung <junesoung412@gmail.com>
2023-02-02 00:15:14 -0700

    less database access by using local variable for weeklybulletin's recent date

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const db = require("./api/firebase.js");
 const app = express();
 const PORT = process.env.port || 3001;
 
+let RECENTDATE;
+
+const getRecentdate = (async () => {
+  try {
+    const snapshot = await db
+      .collection("Misc")
+      .doc("RecentWeeklyBulletin")
+      .get();
+
+    RECENTDATE = snapshot.data().date;
+    console.log(RECENTDATE);
+  } catch {}
+})();
+
 app.use(cors());
 
 app.get("/api/WeeklyUpdate/RecentDate", async (req, res) => {
-  const snapshot = await db
-    .collection("Misc")
-    .doc("RecentWeeklyBulletin")
-    .get();
-
-  res.send(snapshot.data().date);
+  res.send(RECENTDATE);
 });
 
 app.get("/api/WeeklyUpdate/GetBulletin", async (req, res) => {
-  const snapshot = await db
-    .collection("weeklyBulletin")
-    .doc(req.query.date)
-    .get();
-  res.send(snapshot.data().file);
+  try {
+    const snapshot = await db
+      .collection("weeklyBulletin")
+      .doc(req.query.date)
+      .get();
+    res.send(snapshot.data().file);
+  } catch {}
+});
+
+app.put("/api/WeeklyUpdate/PostBulletin", async (req, res) => {
+  const data = {
+    file: req.query.file,
+  };
+  try {
+    await db.collection.doc(req.query.date).set(data);
+    res.send("Success");
+    if (req.query.date > RECENTDATE) {
+      RECENTDATE = req.query.date;
+    }
+  } catch {
+    res.send("Error");
+  }
 });
 
 app.listen(PORT, () => {
